7cde53102a7b99f319219719fb2c388307dc191a
Jorge Andrés Choque <jorge.ch.999santos@gmail.com>
2022-04-24 20:55:24 -0300

    Custom Hooks Agregados

---
+++ src/components/Formulario.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import useMoneda from '../hooks/useMoneda';
+import useCriptomoneda from '../hooks/useCriptomoneda';
+
+const Boton = styled.input`
+    margin-top: 20px;
+    font-weight: bold;
+    font-size: 20px;
+    padding: 10px;
+    background-color: #66a2fe;
+    border: none;
+    width: 100%;
+    border-radius: 10px;
+    color: #fff;
+    transition: background-color .3s ease;
+    
+    &:hover {
+        background-color: #326AC0;
+        cursor: pointer;
+    }
+`;
+
+const Formulario = () => {
+
+    const MONEDAS = [
+        { codigo: 'USD', nombre: 'Dolar de Estados Unidos'},
+        { codigo: 'MXN', nombre: 'Peso Mexicano'},
+        { codigo: 'EUR', nombre: 'Euro'},
+        { codigo: 'GBP', nombre: 'Libra Esterlina'},
+
+    ];
+
+    // Utilizar useMoneda
+    const [ moneda, SelectMonedas ] = useMoneda('Elige tu moneda','', MONEDAS);
+    // Utilizar useCriptomoneda
+    const [ criptoMoneda, SelectCripto ] = useCriptomoneda('Elige tu Criptomoneda','');
+
+    return ( 
+        <form>
+            <SelectMonedas />
+            <SelectCripto />
+            <Boton
+                type='submit'
+                value='Calcular'
+            />
+        </form>
+    );
+}
+ 
+export default Formulario;
